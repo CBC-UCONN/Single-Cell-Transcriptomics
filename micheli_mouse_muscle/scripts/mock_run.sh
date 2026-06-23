6eda5e26f3b1e50a38bf3dfb54d21d472704f6e3
Kerry Cobb <cobbkerry@gmail.com>
2026-06-23 17:15:42 -0400

    mock_run changes

--- micheli_mouse_muscle/scripts/mock_run.sh
+++ micheli_mouse_muscle/scripts/mock_run.sh
@@ -2,16 +2,45 @@
 
 script=$1
 
-if ${script} == "00_download.sh"; then
+if [ ! -d "logs" ]; then
+  mkdir logs
+fi
+
+if [ "$script" == "00_download.sh" ]; then
+  if [ ! -d "../raw_data" ]; then
+    mkdir ../raw_data
+  fi
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/raw_data  ../raw_data
-elif ${script} == "01_fastqc.sh"; then
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/scripts/logs/00_download*.out; do ln -s "$f" logs/; done
+
+elif [ "$script" == "01_fastqc.sh" ]; then
+  if [ ! -d "../results" ]; then
+    mkdir ../results
+  fi
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/results/01_fastqc_raw ../results/01_fastqc_raw
-elif ${script} == "02_multiqc_raw.sh"; then
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/scripts/logs/01_fastqc*.out; do ln -s "$f" logs/; done
+
+elif [ "$script" == "02_multiqc_raw.sh" ]; then
+  if [ ! -d "../results" ]; then
+    mkdir ../results
+  fi
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/results/02_multiqc_raw ../results/02_multiqc_raw
-elif ${script} == "03_star.sh"; then
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/scripts/logs/02_multiqc*.out; do ln -s "$f" logs/; done
+
+elif [ "$script" == "03_star.sh" ]; then
+  if [ ! -d "../results" ]; then
+    mkdir ../results
+  fi
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/results/03_star ../results/03_star
-elif ${script} == "04_multiqc_start.sh"; then
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/scripts/logs/03_star*.out; do ln -s "$f" logs/; done
+
+elif [ "$script" == "04_multiqc_star.sh" ]; then
+  if [ ! -d "../results" ]; then
+    mkdir ../results
+  fi
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/results/04_multiqc_star ../results/04_multiqc_star
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/micheli_mouse_muscle/scripts/logs/04_star*.out; do ln -s "$f" logs/; done
+
 else
     echo "Unknown script: ${script}"
 fi
