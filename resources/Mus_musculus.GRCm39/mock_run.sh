3a15365fec2b3c94169dff826db788f500356e2b
Kerry Cobb <cobbkerry@gmail.com>
2026-06-23 17:02:50 -0400

    Mock_run changes

--- resources/Mus_musculus.GRCm39/mock_run.sh
+++ resources/Mus_musculus.GRCm39/mock_run.sh
@@ -5,10 +5,10 @@ script=$1
 if [ "$script" == "01_download.sh" ]; then
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/Mus_musculus.GRCm39.115.chr.gtf
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/Mus_musculus.GRCm39.dna.primary_assembly.fa
-  ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/01_download*.out 
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/01_download*.out; do ln -s "$f"; done
 elif [ "$script" == "02_star_index.sh" ]; then
   ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/star_index
-  ln -s /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/02_star_index*.out 
+  for f in /core/cbc/tutorials/workshopdirs/Single-Cell-Transcriptomics/resources/Mus_musculus.GRCm39/02_star_index*.out; do ln -s "$f"; done
 else
     echo "Unknown script: $script"
 fi
